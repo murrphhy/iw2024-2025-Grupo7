b7db764a46c45a8a1120de3107ae1cc5c81c2b84
murrphhy <julio.martin.garci@gmail.com>
2024-12-02 23:23:10 +0100

    Login created

--- mysql-init/database.sql
+++ mysql-init/database.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS project_backlog;
 USE project_backlog;
 
-CREATE TABLE `user` (
+CREATE TABLE `users` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `username` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL UNIQUE,
@@ -24,7 +24,7 @@ CREATE TABLE `Project` (
     `start_date` DATE,
     `project_regulations` VARCHAR(255),
     `technical_specification` VARCHAR(255),
-    FOREIGN KEY (applicant_id) REFERENCES user(id)
+    FOREIGN KEY (applicant_id) REFERENCES users(id)
 );
 
 
@@ -33,7 +33,7 @@ CREATE TABLE `Stakeholder_Project` (
     `project_id` BIGINT UNSIGNED NOT NULL,
     `financing` DOUBLE NOT NULL,
     PRIMARY KEY (user_id, project_id),
-    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (project_id) REFERENCES Project(id)
 );
 
@@ -42,7 +42,7 @@ CREATE TABLE `Technician_Project` (
     `project_id` BIGINT UNSIGNED NOT NULL,
     `project_appraisal` BIGINT NOT NULL,
     PRIMARY KEY (user_id, project_id),
-    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (project_id) REFERENCES Project(id)
 );
 
@@ -51,6 +51,6 @@ CREATE TABLE `Support` (
     `project_id` BIGINT UNSIGNED NOT NULL,
     `rating` INT NOT NULL,
     PRIMARY KEY (user_id, project_id),
-    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (project_id) REFERENCES Project(id)
 );
