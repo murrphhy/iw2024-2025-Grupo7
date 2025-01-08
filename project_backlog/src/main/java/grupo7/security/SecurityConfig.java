c85cd532200b1e08de24654fef6c4594a4c3e613
murrphhy <julio.martin.garci@gmail.com>
2025-01-08 03:05:24 +0100

    n

--- project_backlog/src/main/java/grupo7/security/SecurityConfig.java
+++ project_backlog/src/main/java/grupo7/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import grupo7.services.UserService;
 import grupo7.views.login.LoginView;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig extends VaadinWebSecurity {
 
     private final UserDetailsService userDetailsService;
